d5a6c0917c841c122c5e0b2d7e7ff916bf0dd9ad
msw7798 <msw7798@gmail.com>
2021-05-04 19:06:05 +0500

    Final Final

--- realtime_pr/integers/consumers.py
+++ realtime_pr/integers/consumers.py
@@ -3,6 +3,8 @@ import json
 from random import randint
 
 from asyncio import sleep
+from .pn532 import *
+
 
 martin = ['0x4', '0x28', '0xb7', '0xa2', '0x24', '0x69', '0x80']
 screen =  ['0x4', '0x2e', '0xb6', '0xa2', '0x24', '0x69', '0x80']
@@ -16,16 +18,48 @@ md = 0
 mc = 0
 dc = 0 
 bm = 0  
+
 class WSConsumer(AsyncWebsocketConsumer):
+
     async def connect(self):
+        pn532 = PN532_UART(debug=False, reset=20)
+
+        ic, ver, rev, support = pn532.get_firmware_version()
+        print('Found PN532 with firmware version: {0}.{1}'.format(ver, rev))
+
+        # Configure PN532 to communicate with MiFare cards
+        pn532.SAM_configuration()
+
         await self.accept()
         while True:
-            new = randint(22,27)
-            if new==25:
-                f = 0
-            else:
-                f=11               
-            await self.send(json.dumps({'message': randint(1,10),'message2':f}))
+            uid = pn532.read_passive_target(timeout=0.5)
+
+            if uid is None:
+                continue
+            print('Found card with UID:', [hex(i) for i in uid])
+            temp = [hex(i) for i in uid]
+
+            if temp == martin:
+                mr = mr+1
+                print("Martin Mac Aura XB detected")
+            elif temp ==blackmagic:
+                bm = bm+ 1  
+                print("Blackmagic Design URSA is detected")
+            elif temp == screen:
+                sc =sc+1
+                print("55' Screen is detected")
+            elif temp == media:
+                md= md + 1
+                print("Media Server is detected")
+            elif temp ==macbook:
+                mc= mc + 1
+                print("Macbook Pro is detected")
+            elif temp == decimator:
+                dc = dc + 1  
+                print("Decimator MD-HX")
+
+
+            await self.send(json.dumps({'message': mr,'message2':bm,'message3': sc, 'message4':md,'message5':mc,'message6':dc}))
             await sleep(1)
 
     
